de1d20a2005a786c6d50306037d21da3395354a0
Buket Soyhan <buketsoyhan1998@outlook.com>
2023-11-17 16:24:26 +0300

    All algorithms were run 5 times.

--- Heuristic Algorithms/GWO/run.py
+++ Heuristic Algorithms/GWO/run.py
@@ -30,20 +30,37 @@ def run_gwo_trials():
         for pop_size in pop_sizes:
             for num_of_generations in num_of_generations_values:
                 for a in a_values:
-                    fitness_values = GWO(obj_func, lb, ub, 30, pop_size, num_of_generations, [a])
+                    best_fitness_values = []
 
-                    for solution_obj in fitness_values:
-                        results.append({
-                            'Algorithm': "GWO",
-                            'Function': obj_func.__name__,
-                            'Pop_Size': pop_size,
-                            'Num_of_Generations': num_of_generations,
-                            'A_Value': a,
-                            'Best_Fitness': solution_obj.convergence[-1], 
-                        })
+                    for i in range(5):  # 5 iterations
+                        fitness_values = GWO(obj_func, lb, ub, 30, pop_size, num_of_generations, [a])
+
+                        best_fitness_values.append(fitness_values[0].convergence[-1])
+                        print(f"Iteration {i+1} - Best Fitness: {best_fitness_values[-1]}")
+
+                    # Calculate statistics
+                    best_fitness = min(best_fitness_values)
+                    avg_fitness = np.mean(best_fitness_values)
+                    std_dev_fitness = np.std(best_fitness_values)
+
+                    results.append({
+                        'Algorithm': "GWO",
+                        'Function': obj_func.__name__,
+                        'Pop_Size': pop_size,
+                        'Num_of_Generations': num_of_generations,
+                        'A_Value': a,
+                        'Best_Fitness_1': best_fitness_values[0],
+                        'Best_Fitness_2': best_fitness_values[1],
+                        'Best_Fitness_3': best_fitness_values[2],
+                        'Best_Fitness_4': best_fitness_values[3],
+                        'Best_Fitness_5': best_fitness_values[4],
+                        'Best_Fitness': best_fitness,
+                        'Avg_Fitness': avg_fitness,
+                        'Std_Dev_Fitness': std_dev_fitness,
+                    })
 
     df = pd.DataFrame(results)
-    df.to_excel('gwo_results_0.xlsx', index=False)
+    df.to_excel('gwo_results_final.xlsx', index=False)
 
 if __name__ == "__main__":
     run_gwo_trials()
--- Heuristic Algorithms/PSO/run.py
+++ Heuristic Algorithms/PSO/run.py
@@ -4,18 +4,22 @@ from PSO import PSO
 import pandas as pd
 import numpy as np
 
-def pso(obj_func, lb, ub, dim, pop_size, num_of_generations, num_runs=10):
+def pso(obj_func, lb, ub, dim, pop_size, num_of_generations):
     print(f'Running PSO with func={obj_func.__name__}, pop_size={pop_size}, num_of_generations={num_of_generations}')
-    fitness_values = []
-    for _ in range(num_runs):
-        pso_instance = PSO(obj_func, lb, ub, dim, pop_size, num_of_generations)
+    
+    best_fitness_values = []
+    
+    for i in range(5): 
+        pso_instance = PSO(obj_func, lb, ub, dim, pop_size, num_of_generations)  
         best_solution = pso_instance.best
-        fitness_values.append(best_solution)
-    return fitness_values
+        best_fitness_values.append(best_solution)
+        print(f"Iteration {i+1} - Best Fitness: {best_solution}")
+
+    return best_fitness_values
 
 def run_pso_trials():
-    pop_sizes = [100, 250, 500, 1000]
-    num_of_generations_values = [100, 250, 500, 1000]
+    pop_sizes = [100,250,500,1000]
+    num_of_generations_values = [100,250,500,1000]
 
     objective_functions = {
         Functions.ackley: (-32.768, 32.768),
@@ -37,23 +41,29 @@ def run_pso_trials():
 
         for pop_size in pop_sizes:
             for num_of_generations in num_of_generations_values:
-                fitness_values = pso(obj_func, lb, ub, 30, pop_size, num_of_generations)
-                avg_fitness = np.mean(fitness_values)
-                std_dev = np.std(fitness_values)
+                best_fitness_values = pso(obj_func, lb, ub, 30, pop_size, num_of_generations)
+
+                best_fitness = min(best_fitness_values)
+                avg_fitness = np.mean(best_fitness_values)
+                std_dev_fitness = np.std(best_fitness_values)
 
                 results.append({
                     'Algorithm': "PSO",
                     'Function': func_name,
                     'Pop_Size': pop_size,
                     'Num_of_Generations': num_of_generations,
-                    'Best_Fitness': fitness_values[0],  
+                    'Best_Fitness_1': best_fitness_values[0],
+                    'Best_Fitness_2': best_fitness_values[1],
+                    'Best_Fitness_3': best_fitness_values[2],
+                    'Best_Fitness_4': best_fitness_values[3],
+                    'Best_Fitness_5': best_fitness_values[4],
+                    'Best_Fitness': best_fitness,
                     'Avg_Fitness': avg_fitness,
-                    'Std_Dev': std_dev
+                    'Std_Dev_Fitness': std_dev_fitness,
                 })
 
     df = pd.DataFrame(results)
-    df.to_excel('pso_results_2.xlsx', index=False)
+    df.to_excel('pso_results_final.xlsx', index=False)
 
 if __name__ == "__main__":
     run_pso_trials()
-
